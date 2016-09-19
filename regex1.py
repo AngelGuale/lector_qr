f98d56b89f082629f16926e006e0359a3d1cdcc6
AngelGuale <adguale@espol.edu.ec>
2016-09-19 16:23:26 -0500

    Comentarios en codigo

--- regex1.py
+++ regex1.py
@@ -1,7 +1,4 @@
 import re
 
-out2="12_28_1232342a"
-out1="alskal"
-patron=re.compile('[0-9]{2}_[0-9]{2}_[0-9]{6}')
-print patron.match(out2)
-print patron.match(out1)
+patron=re.compile('[0-9]{2}_[0-9]{2}_[0-9]{6}') #patron utilizado para validar el codigo QR
+
--- servicio1.py
+++ servicio1.py
@@ -4,49 +4,44 @@ import subprocess
 import re
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT) #good recognition
-GPIO.setup(17, GPIO.OUT) #always on
-GPIO.setup(15, GPIO.OUT) #bad recognition
+GPIO.setup(18,GPIO.OUT) #good recognition #LED verde para informar de codigo valido
+GPIO.setup(17, GPIO.OUT) #always on # LED AMARILLO, INFORMA QUE EL DISPOSITIVO ESTÁ FUNCIONANDO
+GPIO.setup(15, GPIO.OUT) #bad recognition # LED ROJO, INFORMA UN CODIGO NO VALIDO
 
-codigos_leidos=[]
-patron=re.compile('[0-9]{1,2}_[0-9]{2}_[0-9]{6,12}')
+codigos_leidos=[]#ALMACENA LOS CODIGOS QUE YA HAN SIDO VALIDADOS
+patron=re.compile('[0-9]{1,2}_[0-9]{2}_[0-9]{6,12}') # EXPRESION REGULAR QUE VALIDA LOS CODIGO
 try:
-	cmdping = "zbarcam --prescale=10x10 --nodisplay /dev/video0"
+	cmdping = "zbarcam --prescale=10x10 --nodisplay /dev/video0"  # LLAMADA A LA FUNCION ZBARCAM QUE DETECTA CODIGO QR
 	p = subprocess.Popen(cmdping, shell=True, stdout=subprocess.PIPE)
 	while True:
-		GPIO.output(17, True)
-		GPIO.output(18, False)
-		GPIO.output(15, False)
+		GPIO.output(17, True)  # LED AMARILLO ON
+		GPIO.output(18, False) # LED VERDE OFF
+		GPIO.output(15, False) #LED ROJO OFF
 		out = p.stdout.readline()
-		print "esta es ",out
-		#if out == '' and p.poll() != None:
+		print "Salida del comando ",out
 		if out == '' and p.poll() != None:
-			break
-		if out != '':
-			#sys.stdout.write(out)
-			#sys.stdout.flush()
+			break # si la salida del comando zbar es nula, repite el lazo
+		if out != '': #se procesa la salida
 			try:
-				out2=out.split(":")
+				out2=out.split(":") # se obtiene el dato leido
 				print out2[1]
-				if out2[1] not in codigos_leidos and patron.match(out2[1]):
+				if out2[1] not in codigos_leidos and patron.match(out2[1]): # se valida el dato leido
 					print "aqui se debe prender la luz, todo ok"
-					GPIO.output(18, True)
-					GPIO.output(17, False)
+					GPIO.output(18, True) #LED VERDE ON
+					GPIO.output(17, False) #LED AMARILLO OFF
 					codigos_leidos.append(out2[1])
 				else:
 					print "codigo repetido"
 					print codigos_leidos
-					GPIO.output(18, False)
-					GPIO.output(17, False)
-					GPIO.output(15, True)
+					GPIO.output(18, False) #LED VERDE OFF
+					GPIO.output(17, False) #LED AMARILLO OFF
+					GPIO.output(15, True) #LED ROJO ON
 			except:
-				#print out
-				GPIO.output(18, False)
-				GPIO.output(17, False)
-				GPIO.output(15, True)
-				#pass
-			time.sleep(1)
+				GPIO.output(18, False) #LED VERDE OFF
+				GPIO.output(17, False) #LED AMARILLO OFF
+				GPIO.output(15, True) #LED ROJO ON
+			time.sleep(1)    # TIEMPO DE ENCENDIDO DE LOS LEDS
 except Exception as e:
 	print e
 finally:
-	GPIO.cleanup()
+	GPIO.cleanup() #LINEA PARA APAGAR TODOS LOS LED SI OCURRE ALGUNA INTERRUPCION DEL PROGRAMA
